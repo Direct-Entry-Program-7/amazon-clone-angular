2d73e21337a73b88e10420ad4bc0a560c9c8b5ac
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-10-28 15:30:05 +0530

    Handle cart logic in the parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {DUMMY_DATA} from "./dummy-data";
+import {Item} from "./dto/item";
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,17 @@ import {DUMMY_DATA} from "./dummy-data";
 })
 export class AppComponent {
   items = DUMMY_DATA;
+  cartItems: Array<{code: string, qty: number}> = [];
 
-  updateCart(inCart: number) {
-    console.log(inCart);
+  updateCart(inCart: number, it: Item) {
+
+    const item = this.cartItems.find(i => i.code === it.code);
+
+    if (item){
+      item.qty = inCart;
+    }else{
+      this.cartItems.push({code: it.code, qty: inCart});
+    }
+    console.log(this.cartItems);
   }
 }
