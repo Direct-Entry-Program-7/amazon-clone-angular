31c36b6683fc246be555c9f2f544c930e36494b2
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-01 12:35:43 +0530

    Revamping with ItemService

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {DUMMY_DATA} from "../dummy-data";
 import {Item} from "../dto/item";
+import {ItemService} from "../service/item.service";
 
 @Component({
   selector: 'app-home',
@@ -9,23 +10,27 @@ import {Item} from "../dto/item";
 })
 export class HomeComponent implements OnInit {
 
-  items = DUMMY_DATA;
-  cartItems: Array<{code: string, qty: number}> = [];
+  items: Array<Item>;
+  // cartItems: Array<{code: string, qty: number}> = [];
 
-  updateCart(inCart: number, it: Item) {
-
-    const item = this.cartItems.find(i => i.code === it.code);
-
-    if (item){
-      item.qty = inCart;
-
-      if (item.qty === 0){
-        this.cartItems.splice(this.cartItems.indexOf(item), 1);
-      }
-    }else{
-      this.cartItems.push({code: it.code, qty: inCart});
-    }
+  constructor(private itemService: ItemService) {
+    this.items = itemService.getAllItems();
   }
+
+  // updateCart(inCart: number, it: Item) {
+  //
+  //   const item = this.cartItems.find(i => i.code === it.code);
+  //
+  //   if (item){
+  //     item.qty = inCart;
+  //
+  //     if (item.qty === 0){
+  //       this.cartItems.splice(this.cartItems.indexOf(item), 1);
+  //     }
+  //   }else{
+  //     this.cartItems.push({code: it.code, qty: inCart});
+  //   }
+  // }
 
   ngOnInit(): void {
   }
