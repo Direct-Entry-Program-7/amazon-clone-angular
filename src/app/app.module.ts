10628086231d904d827dbe176e32f60ed558d0bc
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-01 11:20:05 +0530

    Add a new routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { CartItemComponent } from './cart-item/cart-item.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {CartItemComponent} from './cart-item/cart-item.component';
 import {FormsModule} from "@angular/forms";
-import { ItemComponent } from './item/item.component';
-import { HomeComponent } from './home/home.component';
-import { RouterModule, Routes} from "@angular/router";
-import { CartComponent } from './cart/cart.component';
-
-const routes: Routes = [
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'items',
-    component: ItemComponent
-  },
-  {
-    path: 'cart',
-    component: CartComponent
-  },
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: '/home'
-  },
-  {
-    path: '**',
-    redirectTo: '/home'
-  }
-];
+import {ItemComponent} from './item/item.component';
+import {HomeComponent} from './home/home.component';
+import {CartComponent} from './cart/cart.component';
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -46,9 +22,10 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(routes)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
