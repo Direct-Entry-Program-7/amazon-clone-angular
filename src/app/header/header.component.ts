a145f982ed8c30a250ad880613b7916a5e8d5983
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-01 13:04:26 +0530

    Revamp with CartService III

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,9 +13,13 @@ export class HeaderComponent implements OnInit {
   constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    setInterval(()=> {
+      this.updateCart();
+    }, 1000);
   }
 
   updateCart() {
+    console.log("Updating Cart...!")
     this.totalItemsInCart = this.cartService.getTotalItemsInCart();
   }
 }
