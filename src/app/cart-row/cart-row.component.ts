c3724b3efe6fe53524c8cd3bdcc6c13993df0180
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-01 17:17:43 +0530

    Design cart-row.component.html

--- src/app/cart-row/cart-row.component.ts
+++ src/app/cart-row/cart-row.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Item} from "../dto/item";
+import {DUMMY_DATA} from "../dummy-data";
 
 @Component({
   selector: 'app-cart-row',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-row.component.scss']
 })
 export class CartRowComponent implements OnInit {
+
+  item: Item = DUMMY_DATA[0];
 
   constructor() { }
 
