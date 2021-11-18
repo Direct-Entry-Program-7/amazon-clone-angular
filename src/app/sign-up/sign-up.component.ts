53deb850e6e4105db30b7c0c73962b9aaa1d353d
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-18 23:56:08 +0530

    Set the initial focus to name text field

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Title} from "@angular/platform-browser";
 import {ToastrService} from "ngx-toastr";
+import {timer} from "rxjs";
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent implements OnInit, AfterViewInit {
+
+  @ViewChild("txtName")
+  private txtNameElmRef!: ElementRef<HTMLInputElement>;
 
   constructor(private titleService: Title, private toastrService: ToastrService) { }
 
   ngOnInit(): void {
     this.toastrService.clear();
     this.titleService.setTitle("Amazon Registration");
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(()=> this.txtNameElmRef.nativeElement.focus(), 0);
   }
 
 }
